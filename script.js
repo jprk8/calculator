b32b34744593c269e764533c354332d32d32252c
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-29 18:02:49 -0700

    Create calculator framework

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let first;
+let operator;
+let second;
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
+
+console.log(operate('/', 8, 5));
